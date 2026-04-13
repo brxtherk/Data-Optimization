23fa26ea60a755d5030d6796a04df74d71270100
HyeongJe <redtigerhj@gmail.com>
2026-04-14 00:23:30 +0900

    노트북 작업 완료

--- C221015_김형제_1주차_연습.py
+++ C221015_김형제_1주차_연습.py
@@ -168,8 +168,135 @@ else:
     print("The problem does not have an optimal solution.")
                   
 
+#1
+from ortools.linear_solver import pywraplp
 
+# 솔버 생성
+def LinearProgrammingExample():
+    solver = pywraplp.Solver.CreateSolver("GLOP")
+    if not solver:
+        return
+    
+    # 의사결정 변수
+    x = solver.NumVar(0.0, solver.infinity(), "x")
+    y = solver.NumVar(0.0, solver.infinity(), "y")
+    print("Number of Variables =", solver.NumVariables())
 
+    # 제약조건
+    solver.Add(x + 3*y <= 14.0)
+    solver.Add(3*x - y >= 0.0)
+    solver.Add(x - y <= 2.0)
+    print("Number of constraints =", solver.NumConstraints())
+
+    # 목적함수
+    solver.Maximize(3*x + 4*y)
+
+    # 실행
+    status = solver.Solve()
+
+    # 출력
+    if status == pywraplp.Solver.OPTIMAL:
+        print("Solution")
+        print(f"Objective value = {solver.Objective().Value():0.1f}")
+        print(f"x = {x.solution_value():0.1f}")
+        print(f"y = {y.solution_value():0.1f}")
+    else:
+        print("The problem does not have an optimal solution.")
+
+    print(f"Problem solved in {solver.wall_time():d} milliseconds")
+    print(f"Problem solved in {solver.iterations():d} iterations")
+
+LinearProgrammingExample()
+
+#2
+from ortools.linear_solver import pywraplp
+
+# 솔버 생성
+def LinearProgrammingExample():
+    solver = pywraplp.Solver.CreateSolver("SCIP")
+    if not solver:
+        return
+    
+    # 의사결정 변수
+    x = solver.IntVar(0.0, solver.infinity(), "x")
+    y = solver.IntVar(0.0, solver.infinity(), "y")
+    k = solver.IntVar(0.0, solver.infinity(), "k")
+    print("Number of Variables =", solver.NumVariables())
+
+    # 제약조건
+    solver.Add(x + 3*y <= 13.0)
+    solver.Add(3*x - y >= 0.0)
+    solver.Add(x - y <= 2.0)
+    solver.Add(x == 2*k)
+    print("Number of constraints =", solver.NumConstraints())
+
+    # 목적함수
+    solver.Maximize(3*x + 4*y)
+
+    # 실행
+    status = solver.Solve()
+
+    # 출력
+    if status == pywraplp.Solver.OPTIMAL:
+        print("Solution")
+        print(f"Objective value = {solver.Objective().Value():0.1f}")
+        print(f"x = {x.solution_value():0.1f}")
+        print(f"y = {y.solution_value():0.1f}")
+    else:
+        print("The problem does not have an optimal solution.")
+
+    print(f"Problem solved in {solver.wall_time():d} milliseconds")
+    print(f"Problem solved in {solver.iterations():d} iterations")
+
+LinearProgrammingExample()
+
+# 3
+from ortools.linear_solver import pywraplp
+
+data = {}
+data['constraint_coeffs'] = [
+    [5, 7, 9, 2, 1],
+    [18, 4, -9, 10, 12],
+    [4, 7, 3, 8, 5],
+    [5, 13, 16, 3, -7]
+]
+data['bounds'] = [250, 285, 211, 315]
+data['obj_coeffs'] = [7, 8, 2, 9, 6]
+data['num_vars'] = 5
+data['num_constraints'] = 4
+
+# 솔버 생성
+solver = pywraplp.Solver.CreateSolver("SCIP")
+infinity = solver.infinity()
+
+# 의사결정 변수
+x = {}
+for j in range(data['num_vars']):
+    x[j] = solver.IntVar(0.0, infinity, "x[%i]"%j)
+    print("Number of variables", solver.NumVariables())
+
+# 제약조건
+for i in range(data['num_constraints']):
+    constraint_expr = [data['constraint_coeffs'][i][j] * x[j] for j in range(data['num_vars'])]
+    solver.Add(sum(constraint_expr) <= data['bounds'][i])
+    print("Number of constraints =", solver.NumConstraints())
+
+# 목적함수
+objective = solver.Objective()
+obj_expr = [data['obj_coeffs'][j] * x[j] for j in range(data['num_vars'])]
+solver.Maximize(solver.Sum(obj_expr))
+
+# 풀이
+status = solver.Solve()
+
+# 출력
+if status == pywraplp.Solver.OPTIMAL:
+    print("Solution")
+    print(f"Objective value = {objective.Value():0.0f}")
+    for j in range(data['num_vars']):
+        print(f"{x[j].name()} = {x[j].solution_value()}")
+else:
+    print("The problem does not have an optimal solution.")
 
 
 
--- C221015_김형제_2주차_연습.py
+++ C221015_김형제_2주차_연습.py
@@ -225,13 +225,206 @@ for i, nutrient in enumerate(nutrients):
     )
 
 
+# 1
+from ortools.linear_solver import pywraplp
 
+# 솔버 생성
+solver = pywraplp.Solver.CreateSolver("SCIP")
 
+infinity = solver.infinity()
+# 의사결정 변수
+# x: 새콤 주스의 개수, y: 달콤 주스의 개수
 
+x = solver.NumVar(0.0, infinity, "새콤 주스의 개수")
+y = solver.NumVar(0.0, infinity, "달콤 주스의 개수")
+print("Number of variables =", solver.NumVariables())
 
+# 제약조건
+# 남은 키위: 30개, 남은 딸기: 40개
 
+solver.Add(4*x + 2*y <= 30)
+solver.Add(2*x + 6*y <= 40)
+print("Number of constraints =", solver.NumConstraints())
 
+# 목적함수
+# 최대이익: 500*x + 400*y 원
+solver.Maximize(500*x + 400*y)
 
+# 풀이
+print(f"Solving with {solver.SolverVersion()}")
+status = solver.Solve()
+
+# 출력
+if status == pywraplp.Solver.OPTIMAL:
+    print("Solution")
+    print(f"Objective value = {solver.Objective().Value():0.1f}")
+    print(f"x = {x.solution_value():0.1f}")
+    print(f"y = {y.solution_value():0.1f}")
+else:
+    print("The problem does not have an optimal solution.")
+
+print(f"Problem solved in {solver.wall_time()} milliseconds")
+print(f"Problem solved with {solver.iterations()} iterations")
+
+#2
+
+from ortools.linear_solver import pywraplp
+
+data = {}
+data['nutrient_contents'] = [
+    [10, 0, 20, 20, 10, 20],
+    [0, 10, 30, 10, 30, 20]
+]
+data['min_requirements'] = [50, 60]
+data['unit_costs'] = [350, 300, 500, 340, 270, 400]
+data['num_foods'] = 6
+data['num_nutrients'] = 2
+
+# 솔버 생성
+solver = pywraplp.Solver.CreateSolver("SCIP")
+
+infinity = solver.infinity()
+
+# 의사결정 변수
+# x[i] = 비타민 식단에 포함되는 식품 i의 포함량 (100g단위) i=1,2,3,4,5,6
+# x[i] >= 0 (i=1,2,3,4,5,6)
+x = {}
+for j in range(data['num_foods']):
+    x[j] = solver.NumVar(0.0, infinity, "x[%i]"%j)
+print("Number of variables =", solver.NumVariables())
+
+# 제약조건
+# 1일 필수 요구량 50(비타민A), 60(비타민B)
+for i in range(data['num_nutrients']):
+    constraint_expr = [data['nutrient_contents'][i][j] * x[j] for j in range(data['num_foods'])]
+    solver.Add(sum(constraint_expr) >= data['min_requirements'][i])
+print("Number of constraints =", solver.NumConstraints())
+
+# 목적함수
+# 구입비용 최소화: 350*x1 + 300*x2 + 500*x3 + 340*x4 + 270*x5 + 400*x6 (minimize)
+objective = solver.Objective()
+obj_expr = [data['unit_costs'][j] * x[j] for j in range(data['num_foods'])]
+solver.Minimize(solver.Sum(obj_expr))
+
+# 풀이
+print(f"Solving with {solver.SolverVersion()}")
+status = solver.Solve()
+
+# 출력
+if status == pywraplp.Solver.OPTIMAL:
+    print("\n==========Solution==========")
+    print(f"Objective value = {objective.Value():0.1f}")
+    for j in range(data['num_foods']):
+        print(f"{x[j].name()} = {x[j].solution_value()}")
+else:
+    print("The problem does not have an optimal solution.")
+
+print(f"Problem solved in {solver.wall_time()} milliseconds")
+print(f"Problem solved with {solver.iterations()} iterations")
+
+# 3
+
+from ortools.linear_solver import pywraplp
+
+# 솔버 생성
+solver = pywraplp.Solver.CreateSolver("SCIP")
+
+data = {}
+data['nut_amounts'] = [  # 단위: 온즈
+    [15, 10, 6, 2],
+    [1, 6, 10, 14]
+]
+data['prices'] = [2, 3, 4, 5]
+data['supply'] = [750, 250]  # 단위: 파운드
+data['num_products'] = 4
+data['num_nuts'] = 2
+data['products_name'] = ["Pawn", "Knight", "Bishop", "King"]
+
+infinity = solver.infinity()
+
+# 의사결정 변수
+# x[i] = 매일 생산해야 하는 제품의 파운드 양(x[i] >= 0 / i=1,2,3,4)
+# x[1] = Pawn, x[2] = Knight, x[3] = Bishop, x[4] = King
+x = {}
+for j in range(data['num_products']):
+    x[j] = solver.NumVar(0.0, infinity, data['products_name'][j])
+print("Number of variables =", solver.NumVariables())
+
+# 제약조건
+# 하루 최대 공급 견과 양: 땅콩 750파운드, 캐슈 250파운드
+for i in range(data['num_nuts']):
+    constraint_expr = [data['nut_amounts'][i][j] * x[j] for j in range(data['num_products'])]
+    solver.Add(sum(constraint_expr) <= data['supply'][i]*16)  # 단위 통일(1파운드 = 16온즈)
+print("Number of constraints =", solver.NumConstraints())
+
+# 목적함수
+# 총 이익 최대화: $2*x[1] + $3*x[2] + $4*x[3] + $5*x[5] (Maximize)
+obj_expr = [data['prices'][j] * x[j] for j in range(data['num_products'])]
+solver.Maximize(solver.Sum(obj_expr))
+
+# 풀이
+print(f"Solving with {solver.SolverVersion()}")
+status = solver.Solve()
+
+# 출력
+if status == pywraplp.Solver.OPTIMAL:
+    print("\n----------Solution----------")
+    print(f"Objective value = {solver.Objective().Value():0.2f}")
+    for j in range(data['num_products']):
+        print(f"{x[j].name()} = {x[j].solution_value()}파운드")
+else:
+    print("The problem does not have an optimal solution.")
+
+print(f"Problem solved in {solver.wall_time()} milliseconds")
+print(f"Problem solved with {solver.iterations()} iterations")
+
+# 스티글러 식단
+
+from ortools.linear_solver import pywraplp
+from or2_4_data import *
+
+# 솔버 객체 생성
+solver = pywraplp.Solver.CreateSolver("SCIP")
+
+# 의사결정 변수
+foods = [solver.NumVar(0.0, solver.infinity(), item[0]) for item in data]
+print("Number of variables =", solver.NumVariables())
+
+# 제약조건
+constraints = []
+for i, nutrient in enumerate(nutrients):
+    constraints.append(solver.Constraint(nutrient[1], solver.infinity()))
+    for j, item in enumerate(data):
+        constraints[i].SetCoefficient(foods[j], item[i+3])
+print("Number of constraints =", solver.NumConstraints())
+
+# 목적함수 
+# objective = solver.Objective()
+# for j in range(len(foods)):
+#     objective.SetCoefficient(foods[j], data[j][2])
+# objective.SetMinimization()
+
+objective = solver.Objective()
+for j, food in enumerate(foods):
+    objective.SetCoefficient(foods[j],data[j][2])
+objective.SetMinimization()
+
+# 풀이
+print(f"Solving with {solver.SolverVersion()}")
+status = solver.Solve()
+
+# 출력
+if status == pywraplp.Solver.OPTIMAL:
+    print("\n==========Solution==========")
+    print(f"Objective value = {objective.Value():.4f} cents")
+    for j, food in enumerate(foods):
+        if foods[j].solution_value() > 0.0:
+            print(f"{foods[j].name()} = {foods[j].solution_value():0.4f}")
+else:
+    print("The problem does not have an optimal solution.")
+
+print(f"Problem solved in {solver.wall_time()} milliseconds")
+print(f"Problem solved with {solver.iterations()} iterations")           
 
 
 
